95cac4807754f01bf9fe646386a47f6fa19dd6d5
harishchandragiri <giriharishchandra9999@gmail.com>
2024-07-12 23:35:04 +0545

    wiewport height added in navbar and NotFound

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div className='flex justify-center items-center min-h-screen mt-[55px] sm:mt-[75px] bg-slate-500 ' >
+    <div className='flex justify-center items-center h-[90vh] bg-slate-500 ' >
         <div className='font-bold text-3xl'>Page NotFound</div>
     </div>
   )
